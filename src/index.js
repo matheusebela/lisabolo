6063f234f4a2039770fac19ed95cfe4fa47309bd
Matheus Odilon <matheusodilon@gmail.com>
2022-01-19 00:42:45 -0300

    Adicionando os components genericos de *Header* e *Footer* ao projeto

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ import './styles/bakery-styles.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './components/Home';
 import ElementsNative from './components/Elements/Elements.native';
+import Header from './components/Header';
+import Footer from './components/Footer/Footer.container';
 
 const routing = (
   <>
     <Router>
+      <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/elements" element={<ElementsNative />} />
       </Routes>
+      <Footer />
     </Router>
   </>
   )
