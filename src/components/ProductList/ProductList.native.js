ce4e4c92881adb1f6b3b873cb77d8aee59956704
Matheus Odilon <matheusodilon@gmail.com>
2022-01-21 00:30:52 -0300

    Criando component que será usado para a listagens dos produtos

---
+++ src/components/ProductList/ProductList.native.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductListNative = () => {
+  return (<></>);
+};
+
+export default ProductListNative;
---
+++ src/components/ProductList/index.js
@@ -0,0 +1,3 @@
+import ProductList from "./ProductList.container";
+
+export default ProductList;
